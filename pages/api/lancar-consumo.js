b3ee0f16cca9d5562032bdcd9a6affc6f6b58463
helderwlima-dev <helderwlima@gmail.com>
2026-02-19 12:50:44 -0300

    Update lancar-consumo.js

--- pages/api/lancar-consumo.js
+++ pages/api/lancar-consumo.js
@@ -1,3 +1,13 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Max-Age", "86400");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(process.env.SUPABASEURL, process.env.SUPABASEKEY);
